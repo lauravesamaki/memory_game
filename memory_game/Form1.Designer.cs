acdda89fa0b1be051971a61b48a822a640bab4b7
Laura Vesamäki <laura.vesamaki@live.fi>
2023-08-29 11:16:20 +0300

    edited

---
+++ memory_game/Form1.Designer.cs
@@ -0,0 +1,383 @@
+﻿namespace memory_game
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.btnStartGame = new System.Windows.Forms.Button();
+			this.lblClickMe = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.button9 = new System.Windows.Forms.Button();
+			this.button10 = new System.Windows.Forms.Button();
+			this.button11 = new System.Windows.Forms.Button();
+			this.button12 = new System.Windows.Forms.Button();
+			this.button13 = new System.Windows.Forms.Button();
+			this.button14 = new System.Windows.Forms.Button();
+			this.button15 = new System.Windows.Forms.Button();
+			this.button16 = new System.Windows.Forms.Button();
+			this.btnNewGame = new System.Windows.Forms.Button();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.lblTimer = new System.Windows.Forms.Label();
+			this.btnStop = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// btnStartGame
+			// 
+			this.btnStartGame.BackColor = System.Drawing.Color.MediumAquamarine;
+			this.btnStartGame.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnStartGame.Font = new System.Drawing.Font("Segoe UI Light", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnStartGame.Location = new System.Drawing.Point(400, 111);
+			this.btnStartGame.Name = "btnStartGame";
+			this.btnStartGame.Size = new System.Drawing.Size(195, 59);
+			this.btnStartGame.TabIndex = 0;
+			this.btnStartGame.Text = "Start Game";
+			this.btnStartGame.UseVisualStyleBackColor = false;
+			this.btnStartGame.Click += new System.EventHandler(this.btnStartGame_Click);
+			// 
+			// lblClickMe
+			// 
+			this.lblClickMe.AutoSize = true;
+			this.lblClickMe.Font = new System.Drawing.Font("Segoe UI Light", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblClickMe.Location = new System.Drawing.Point(365, 36);
+			this.lblClickMe.Name = "lblClickMe";
+			this.lblClickMe.Size = new System.Drawing.Size(275, 54);
+			this.lblClickMe.TabIndex = 1;
+			this.lblClickMe.Text = "Memory Game";
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.Black;
+			this.button1.Font = new System.Drawing.Font("Segoe UI Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(294, 271);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(100, 100);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.Color.Black;
+			this.button2.Font = new System.Drawing.Font("Segoe UI Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.Location = new System.Drawing.Point(395, 271);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(100, 100);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.BackColor = System.Drawing.Color.Black;
+			this.button3.Font = new System.Drawing.Font("Segoe UI Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button3.Location = new System.Drawing.Point(496, 271);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(100, 100);
+			this.button3.TabIndex = 4;
+			this.button3.Text = "button3";
+			this.button3.UseVisualStyleBackColor = false;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button4
+			// 
+			this.button4.BackColor = System.Drawing.Color.Black;
+			this.button4.Font = new System.Drawing.Font("Segoe UI Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button4.Location = new System.Drawing.Point(597, 271);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(100, 100);
+			this.button4.TabIndex = 5;
+			this.button4.Text = "button4";
+			this.button4.UseVisualStyleBackColor = false;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// button5
+			// 
+			this.button5.BackColor = System.Drawing.Color.Black;
+			this.button5.Font = new System.Drawing.Font("Segoe UI Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button5.Location = new System.Drawing.Point(294, 372);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(100, 100);
+			this.button5.TabIndex = 6;
+			this.button5.Text = "button5";
+			this.button5.UseVisualStyleBackColor = false;
+			this.button5.Click += new System.EventHandler(this.button5_Click);
+			// 
+			// button6
+			// 
+			this.button6.BackColor = System.Drawing.Color.Black;
+			this.button6.Font = new System.Drawing.Font("Segoe UI Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button6.Location = new System.Drawing.Point(395, 372);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(100, 100);
+			this.button6.TabIndex = 7;
+			this.button6.Text = "button6";
+			this.button6.UseVisualStyleBackColor = false;
+			this.button6.Click += new System.EventHandler(this.button6_Click);
+			// 
+			// button7
+			// 
+			this.button7.BackColor = System.Drawing.Color.Black;
+			this.button7.Font = new System.Drawing.Font("Segoe UI Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button7.Location = new System.Drawing.Point(496, 372);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(100, 100);
+			this.button7.TabIndex = 8;
+			this.button7.Text = "button7";
+			this.button7.UseVisualStyleBackColor = false;
+			this.button7.Click += new System.EventHandler(this.button7_Click);
+			// 
+			// button8
+			// 
+			this.button8.BackColor = System.Drawing.Color.Black;
+			this.button8.Font = new System.Drawing.Font("Segoe UI Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button8.Location = new System.Drawing.Point(597, 372);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(100, 100);
+			this.button8.TabIndex = 9;
+			this.button8.Text = "button8";
+			this.button8.UseVisualStyleBackColor = false;
+			this.button8.Click += new System.EventHandler(this.button8_Click);
+			// 
+			// button9
+			// 
+			this.button9.BackColor = System.Drawing.Color.Black;
+			this.button9.Font = new System.Drawing.Font("Segoe UI Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button9.Location = new System.Drawing.Point(294, 473);
+			this.button9.Name = "button9";
+			this.button9.Size = new System.Drawing.Size(100, 100);
+			this.button9.TabIndex = 10;
+			this.button9.Text = "button9";
+			this.button9.UseVisualStyleBackColor = false;
+			this.button9.Click += new System.EventHandler(this.button9_Click);
+			// 
+			// button10
+			// 
+			this.button10.BackColor = System.Drawing.Color.Black;
+			this.button10.Font = new System.Drawing.Font("Segoe UI Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button10.Location = new System.Drawing.Point(395, 473);
+			this.button10.Name = "button10";
+			this.button10.Size = new System.Drawing.Size(100, 100);
+			this.button10.TabIndex = 11;
+			this.button10.Text = "button10";
+			this.button10.UseVisualStyleBackColor = false;
+			this.button10.Click += new System.EventHandler(this.button10_Click);
+			// 
+			// button11
+			// 
+			this.button11.BackColor = System.Drawing.Color.Black;
+			this.button11.Font = new System.Drawing.Font("Segoe UI Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button11.Location = new System.Drawing.Point(496, 473);
+			this.button11.Name = "button11";
+			this.button11.Size = new System.Drawing.Size(100, 100);
+			this.button11.TabIndex = 12;
+			this.button11.Text = "button11";
+			this.button11.UseVisualStyleBackColor = false;
+			this.button11.Click += new System.EventHandler(this.button11_Click);
+			// 
+			// button12
+			// 
+			this.button12.BackColor = System.Drawing.Color.Black;
+			this.button12.Font = new System.Drawing.Font("Segoe UI Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button12.Location = new System.Drawing.Point(597, 473);
+			this.button12.Name = "button12";
+			this.button12.Size = new System.Drawing.Size(100, 100);
+			this.button12.TabIndex = 13;
+			this.button12.Text = "button12";
+			this.button12.UseVisualStyleBackColor = false;
+			this.button12.Click += new System.EventHandler(this.button12_Click);
+			// 
+			// button13
+			// 
+			this.button13.BackColor = System.Drawing.Color.Black;
+			this.button13.Font = new System.Drawing.Font("Segoe UI Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button13.Location = new System.Drawing.Point(294, 574);
+			this.button13.Name = "button13";
+			this.button13.Size = new System.Drawing.Size(100, 100);
+			this.button13.TabIndex = 14;
+			this.button13.Text = "button13";
+			this.button13.UseVisualStyleBackColor = false;
+			this.button13.Click += new System.EventHandler(this.button13_Click);
+			// 
+			// button14
+			// 
+			this.button14.BackColor = System.Drawing.Color.Black;
+			this.button14.Font = new System.Drawing.Font("Segoe UI Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button14.Location = new System.Drawing.Point(395, 574);
+			this.button14.Name = "button14";
+			this.button14.Size = new System.Drawing.Size(100, 100);
+			this.button14.TabIndex = 15;
+			this.button14.Text = "button14";
+			this.button14.UseVisualStyleBackColor = false;
+			this.button14.Click += new System.EventHandler(this.button14_Click);
+			// 
+			// button15
+			// 
+			this.button15.BackColor = System.Drawing.Color.Black;
+			this.button15.Font = new System.Drawing.Font("Segoe UI Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button15.Location = new System.Drawing.Point(496, 574);
+			this.button15.Name = "button15";
+			this.button15.Size = new System.Drawing.Size(100, 100);
+			this.button15.TabIndex = 16;
+			this.button15.Text = "button15";
+			this.button15.UseVisualStyleBackColor = false;
+			this.button15.Click += new System.EventHandler(this.button15_Click);
+			// 
+			// button16
+			// 
+			this.button16.BackColor = System.Drawing.Color.Black;
+			this.button16.Font = new System.Drawing.Font("Segoe UI Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button16.Location = new System.Drawing.Point(597, 574);
+			this.button16.Name = "button16";
+			this.button16.Size = new System.Drawing.Size(100, 100);
+			this.button16.TabIndex = 17;
+			this.button16.Text = "button16";
+			this.button16.UseVisualStyleBackColor = false;
+			this.button16.Click += new System.EventHandler(this.button16_Click);
+			// 
+			// btnNewGame
+			// 
+			this.btnNewGame.BackColor = System.Drawing.Color.MediumAquamarine;
+			this.btnNewGame.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnNewGame.Font = new System.Drawing.Font("Segoe UI Light", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnNewGame.Location = new System.Drawing.Point(400, 798);
+			this.btnNewGame.Name = "btnNewGame";
+			this.btnNewGame.Size = new System.Drawing.Size(195, 59);
+			this.btnNewGame.TabIndex = 18;
+			this.btnNewGame.Text = "New Game";
+			this.btnNewGame.UseVisualStyleBackColor = false;
+			this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+			// 
+			// timer1
+			// 
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// lblTimer
+			// 
+			this.lblTimer.AutoSize = true;
+			this.lblTimer.Font = new System.Drawing.Font("Segoe UI Light", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblTimer.Location = new System.Drawing.Point(294, 205);
+			this.lblTimer.Name = "lblTimer";
+			this.lblTimer.Size = new System.Drawing.Size(101, 45);
+			this.lblTimer.TabIndex = 19;
+			this.lblTimer.Text = "Time: ";
+			// 
+			// btnStop
+			// 
+			this.btnStop.BackColor = System.Drawing.Color.MediumAquamarine;
+			this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnStop.Font = new System.Drawing.Font("Segoe UI Light", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnStop.Location = new System.Drawing.Point(400, 709);
+			this.btnStop.Name = "btnStop";
+			this.btnStop.Size = new System.Drawing.Size(195, 59);
+			this.btnStop.TabIndex = 20;
+			this.btnStop.Text = "Stop";
+			this.btnStop.UseVisualStyleBackColor = false;
+			this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(805, 372);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(51, 20);
+			this.label1.TabIndex = 21;
+			this.label1.Text = "label1";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1109, 880);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.btnStop);
+			this.Controls.Add(this.lblTimer);
+			this.Controls.Add(this.btnNewGame);
+			this.Controls.Add(this.button16);
+			this.Controls.Add(this.button15);
+			this.Controls.Add(this.button14);
+			this.Controls.Add(this.button13);
+			this.Controls.Add(this.button12);
+			this.Controls.Add(this.button11);
+			this.Controls.Add(this.button10);
+			this.Controls.Add(this.button9);
+			this.Controls.Add(this.button8);
+			this.Controls.Add(this.button7);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.lblClickMe);
+			this.Controls.Add(this.btnStartGame);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Name = "Form1";
+			this.Text = "Memory Game";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnStartGame;
+		private System.Windows.Forms.Label lblClickMe;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Button button9;
+		private System.Windows.Forms.Button button10;
+		private System.Windows.Forms.Button button11;
+		private System.Windows.Forms.Button button12;
+		private System.Windows.Forms.Button button13;
+		private System.Windows.Forms.Button button14;
+		private System.Windows.Forms.Button button15;
+		private System.Windows.Forms.Button button16;
+		private System.Windows.Forms.Button btnNewGame;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Label lblTimer;
+		private System.Windows.Forms.Button btnStop;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ memory_game/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace memory_game
+{
+	public partial class Form1 : Form
+	{
+		public Random random = new Random();
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		//list of numbers to be shuffled
+		List<string> numbers = new List<string>()
+		{
+			"1", "1", "2", "2", "3", "3", "4", "4", "5", "5",
+			"6", "6", "7", "7", "8", "8"
+		};
+
+		//list of buttons
+		List<Button> buttons = new List<Button>();
+
+		//list of clicked buttons
+		List<Button> clickedButtons = new List<Button>();
+
+		//click event for all buttons
+		private void button_Click(object sender, EventArgs e)
+		{
+			Button button = (Button)sender;
+			button.BackColor = Color.White;
+			button.Enabled = false;
+
+			if (clickedButtons.Count < 2)
+			{
+				clickedButtons.Add(button);
+
+				if (clickedButtons.Count == 2)
+				{
+					//disable all other buttons
+					foreach (Button b in buttons)
+					{
+						if (b != clickedButtons[0] && b != clickedButtons[1])
+						{
+							b.Enabled = false;
+						}
+					}
+
+					if (clickedButtons[0].Text == clickedButtons[1].Text)
+					{
+						clickedButtons[0].BackColor = Color.Green;
+						clickedButtons[1].BackColor = Color.Green;
+
+						foreach (Button b in buttons)
+						{
+							if (b != clickedButtons[0] && b != clickedButtons[1])
+							{
+								b.Enabled = true;
+							}
+						}
+
+						clickedButtons.Clear();
+					}
+					else
+					{
+						System.Timers.Timer timer = new System.Timers.Timer();
+						timer.Interval = 1000;
+						timer.Start();
+
+						timer.Elapsed += (s, args) =>
+						{
+							clickedButtons[0].BackColor = Color.Black;
+							clickedButtons[1].BackColor = Color.Black;
+							clickedButtons[0].Enabled = true;
+							clickedButtons[1].Enabled = true;
+
+							foreach (Button b in buttons)
+							{
+								if (b != clickedButtons[0] && b != clickedButtons[1])
+								{
+									b.Enabled = true;
+								}
+							}
+
+							clickedButtons.Clear();
+							timer.Stop();
+						};
+					}
+				}
+			}
+		}
+
+		//button to start the game
+		private void btnStartGame_Click(object sender, EventArgs e)
+		{
+			List<string> newOrderNumbers = new List<string>();
+
+			for (int i = 0; i < numbers.Count; i++)
+			{
+				int index = random.Next(numbers.Count);
+				
+				//check if the number is already in the list twice
+				if (newOrderNumbers.Count(x => x == numbers[index]) < 2)
+				{
+					newOrderNumbers.Add(numbers[index]);
+				}
+				else
+				{
+					i--;
+				}
+			}
+
+			//find all buttons where text starts with "button" and add them to the list
+			buttons = this.Controls.OfType<Button>().Where(x => x.Name.StartsWith("button")).ToList();
+
+			for (int i = 0; i < buttons.Count; i++)
+			{
+				buttons[i].Text = newOrderNumbers[i];
+				buttons[i].Enabled = true;
+			}
+
+			//add click event to all buttons
+			foreach (Button b in buttons)
+			{
+				b.Click += button_Click;
+			}
+
+			//start timer
+			timer1.Start();
+		}
+
+		//buttons for the numbers
+		private void button1_Click(object sender, EventArgs e)
+		{			
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{			
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+		}
+
+		private void button5_Click(object sender, EventArgs e)
+		{
+		}
+
+		private void button6_Click(object sender, EventArgs e)
+		{
+		}
+
+		private void button7_Click(object sender, EventArgs e)
+		{
+		}
+
+		private void button8_Click(object sender, EventArgs e)
+		{
+		}
+
+		private void button9_Click(object sender, EventArgs e)
+		{
+		}
+
+		private void button10_Click(object sender, EventArgs e)
+		{
+		}
+
+		private void button11_Click(object sender, EventArgs e)
+		{
+		}
+
+		private void button12_Click(object sender, EventArgs e)
+		{
+		}
+
+		private void button13_Click(object sender, EventArgs e)
+		{
+		}
+
+		private void button14_Click(object sender, EventArgs e)
+		{
+		}
+
+		private void button15_Click(object sender, EventArgs e)
+		{
+		}
+
+		private void button16_Click(object sender, EventArgs e)
+		{
+		}
+
+		//button to start a new game
+		private void btnNewGame_Click(object sender, EventArgs e)
+		{
+			//clear all buttons
+			foreach (Button button in buttons)
+			{
+				button.Text = "";
+				button.BackColor = Color.Black;
+				button.Enabled = false;
+			}
+			time = 0;
+			lblTimer.Text = "Time: 0 s";
+		}
+
+		public int time { get; private set; }
+
+		//timer to count the time
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			time++;
+
+			int seconds = time / 10;
+			lblTimer.Text = "Time: " + seconds.ToString() + " s";
+		}
+
+		//button to stop the timer
+		private void btnStop_Click(object sender, EventArgs e)
+		{
+			//stop timer
+			timer1.Stop();
+		}
+	}
+}
